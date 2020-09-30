2b1e30b4ffb00725e682b70c165d06241213e8ee
m213228 <m213228@usna.edu>
2020-09-29 21:23:08 -0400

    done

--- 99bottles.cs
+++ 99bottles.cs
@@ -4,38 +4,68 @@
 using System;
 using System.Linq;
 using System.Text;
+using Gtk;
 
 namespace NinetyNineBottles
 {
-  class Beer
-  {
-    static void Main(string[] args)
+    class Beer
     {
-        StringBuilder beerLyric = new StringBuilder();
-        string nl = System.Environment.NewLine;
-
-        var beers =
-            (from n in Enumerable.Range(0, 100)
-             select new { 
-               Say =  n == 0 ? "No more bottles" : 
-                     (n == 1 ? "1 bottle" : n.ToString() + " bottles"),
-               Next = n == 1 ? "no more bottles" : 
-                     (n == 0 ? "99 bottles" : 
-                     (n == 2 ? "1 bottle" : n.ToString() + " bottles")),
-               Action = n == 0 ? "Go to the store and buy some more" : 
-                                 "Print it out, stand up and shout"
-             });
-
-        foreach (var beer in beers)
-        {
-            beerLyric.AppendFormat("{0} of text on the screen, {1} of text.{2}",
-                                    beer.Say, beer.Say.ToLower(), nl);
-            beerLyric.AppendFormat("{0}, {1} of text on the screen.{2}", 
-                                    beer.Action, beer.Next, nl);
-            beerLyric.AppendLine();
+        public VBox f;
+        public Entry e;
+        public Button b;
+        public Beer() {
+            f = new VBox();
+            Label text = new Label();
+            text.Text = "How many lines?";
+            f.Add(text);
+            e = new Entry();
+            b = new Button("Enter");
+            b.Clicked += new EventHandler(EnterHandler);
+            f.Add(e);
+            f.Add(b);
         }
-        Console.WriteLine(beerLyric.ToString());
-        Console.ReadLine();
+        void EnterHandler(object obj, EventArgs args){
+            int lines = Convert.ToInt32(e.Text);
+            WriteBeer(lines);
+        }
+
+
+        static void Main(string[] args)
+        {
+            Application.Init();
+            Window myWin = new Window("Prompt");
+            Beer B = new Beer();
+            myWin.Add(B.f);
+            myWin.ShowAll();
+            Application.Run();
+
+        }
+
+        static void WriteBeer(int lines) {
+            StringBuilder beerLyric = new StringBuilder();
+            string nl = System.Environment.NewLine;
+            var beers =
+                (from n in Enumerable.Range(0, lines)
+                 select new { 
+                 Say =  n == 0 ? "No more lines" : 
+                 (n == 1 ? "1 line" : n.ToString() + " lines"),
+                 Next = n == 1 ? "no more lines" : 
+                 (n == 0 ? "99 lines" : 
+                  (n == 2 ? "1 line" : n.ToString() + " lines")),
+                 Action = n == 0 ? "Go to the store and buy some more" : 
+                 "Print it out, stand up and shout"
+                 });
+
+            foreach (var beer in beers)
+            {
+                beerLyric.AppendFormat("{0} of text on the screen, {1} of text.{2}",
+                        beer.Say, beer.Say.ToLower(), nl);
+                beerLyric.AppendFormat("{0}, {1} of text on the screen.{2}", 
+                        beer.Action, beer.Next, nl);
+                beerLyric.AppendLine();
+            }
+            Console.WriteLine(beerLyric.ToString());
+            Console.ReadLine();
+        }
     }
-  }
 }
