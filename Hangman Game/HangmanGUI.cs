4898635ac234a9fefcc2eb8ef5b8d9876074f660
MIDN S.E. Moriarty <m214728@usna.edu>
2020-10-28 08:58:18 -0700

    Finished a long learning session on the basics of Windows Forms

--- Hangman Game/HangmanGUI.cs
+++ Hangman Game/HangmanGUI.cs
@@ -7,21 +7,46 @@ namespace Hangman
 {
     public class HangmanForm : Form
     {
-        public Button button;
+        private Button button;
+        private Button button2;
 
         public HangmanForm(){
+            this.StartPosition = FormStartPosition.CenterScreen;
             create_button();
+            create_button2();
+            this.Size = new Size(500, 500);
         }
 
         //helper method to initialize the button
         private void create_button(){
             button = new Button();
             button.Size = new Size(40, 40);
-            button.Location = new Point(30, 30);
+            button.Location = new Point(this.Size.Width - 40, 30);
             button.Text = "Click me";
             this.Controls.Add(button);
             button.Click += new EventHandler(button_Click);
         }
+        
+        //helper method to initialize the button
+        private void create_button2(){
+            button2 = new Button();
+            button2.Size = new Size(40, 40);
+            button2.Location = new Point(50, 30);
+            button2.Padding = new Padding(10);
+            button2.Text = "Click me";
+            this.Controls.Add(button2);
+            button2.Click += new EventHandler(button_Click);
+        }
+
+        private void update_Button(){
+            button.Location = new Point(this.Size.Width - 41, 30);
+        } 
+
+        protected override void OnResize(EventArgs e){
+            base.OnResize(e);
+            update_Button();
+        }
+
 
         private void button_Click(object sender, EventArgs e){
             MessageBox.Show("Hey ya'll");
